a64c5ef264433541891a0ce3269a59377c28eb83
Kaleb <kaleb.taylor1976@gmail.com>
2023-03-19 01:53:06 -0700

    refactor of code

--- src/Space-age.js
+++ src/Space-age.js
@@ -1,58 +1,60 @@
-export default function Age(age, ageDelta, ageDeltaPast, ageDeltaFuture) {
-  this.age = age;
-  this.ageDelta = ageDelta;
-  this.ageDeltaPast = ageDeltaPast;
-  this.ageDeltaFuture = ageDeltaFuture;
+export default class Age {
+  constructor(age, ageDelta, ageDeltaPast, ageDeltaFuture) {
+    this.age = age;
+    this.ageDelta = ageDelta;
+    this.ageDeltaPast = ageDeltaPast;
+    this.ageDeltaFuture = ageDeltaFuture;
+  }
+
+  mercury() {
+    let numb = ((1/.24) * this.age).toFixed(2);
+    let ageAlt = parseFloat(numb);
+
+    let ageDeltaPast = ((1/.24) * (this.age - this.ageDelta)).toFixed(2);
+    this.ageDeltaPast = parseFloat(ageDeltaPast);
+
+    let ageDeltaFuture = ((1/.24) * (this.ageDelta - this.age)).toFixed(2);
+    this.ageDeltaFuture = parseFloat(ageDeltaFuture);
+
+    return ageAlt;
+  }
+
+  venus() {
+    let numb = ((1/.62) * this.age).toFixed(2);
+    let ageAlt = parseFloat(numb);
+
+    let ageDeltaPast = ((1/.62) * (this.age - this.ageDelta)).toFixed(2);
+    this.ageDeltaPast = parseFloat(ageDeltaPast);
+
+    let ageDeltaFuture = ((1/.62) * (this.ageDelta - this.age)).toFixed(2);
+    this.ageDeltaFuture = parseFloat(ageDeltaFuture);
+
+    return ageAlt;
+  }
+
+  mars() {
+    let numb = ((1/1.88) * this.age).toFixed(2);
+    let ageAlt = parseFloat(numb);
+
+    let ageDeltaPast = ((1/1.88) * (this.age - this.ageDelta)).toFixed(2);
+    this.ageDeltaPast = parseFloat(ageDeltaPast);
+
+    let ageDeltaFuture = ((1/1.88) * (this.ageDelta - this.age)).toFixed(2);
+    this.ageDeltaFuture = parseFloat(ageDeltaFuture);
+
+    return ageAlt;
+  }
+
+  jupiter() {
+    let numb = ((1/11.86) * this.age).toFixed(2);
+    let ageAlt = parseFloat(numb);
+
+    let ageDeltaPast = ((1/11.86) * (this.age - this.ageDelta)).toFixed(2);
+    this.ageDeltaPast = parseFloat(ageDeltaPast);
+
+    let ageDeltaFuture = ((1/11.86) * (this.ageDelta - this.age)).toFixed(2);
+    this.ageDeltaFuture = parseFloat(ageDeltaFuture);
+
+    return ageAlt;
+  }
 }
-
-Age.prototype.mercury = function() {
-  let numb = ((1/.24) * this.age).toFixed(2);
-  let ageAlt = parseFloat(numb);
-
-  let ageDeltaPast = ((1/.24) * (this.age - this.ageDelta)).toFixed(2);
-  this.ageDeltaPast = parseFloat(ageDeltaPast);
-
-  let ageDeltaFuture = ((1/.24) * (this.ageDelta - this.age)).toFixed(2);
-  this.ageDeltaFuture = parseFloat(ageDeltaFuture);
-
-  return ageAlt;
-};
-
-Age.prototype.venus = function() {
-  let numb = ((1/.62) * this.age).toFixed(2);
-  let ageAlt = parseFloat(numb);
-
-  let ageDeltaPast = ((1/.62) * (this.age - this.ageDelta)).toFixed(2);
-  this.ageDeltaPast = parseFloat(ageDeltaPast);
-
-  let ageDeltaFuture = ((1/.62) * (this.ageDelta - this.age)).toFixed(2);
-  this.ageDeltaFuture = parseFloat(ageDeltaFuture);
-
-  return ageAlt;
-};
-
-Age.prototype.mars = function() {
-  let numb = ((1/1.88) * this.age).toFixed(2);
-  let ageAlt = parseFloat(numb);
-
-  let ageDeltaPast = ((1/1.88) * (this.age - this.ageDelta)).toFixed(2);
-  this.ageDeltaPast = parseFloat(ageDeltaPast);
-
-  let ageDeltaFuture = ((1/1.88) * (this.ageDelta - this.age)).toFixed(2);
-  this.ageDeltaFuture = parseFloat(ageDeltaFuture);
-
-  return ageAlt;
-};
-
-Age.prototype.jupiter = function() {
-  let numb = ((1/11.86) * this.age).toFixed(2);
-  let ageAlt = parseFloat(numb);
-
-  let ageDeltaPast = ((1/11.86) * (this.age - this.ageDelta)).toFixed(2);
-  this.ageDeltaPast = parseFloat(ageDeltaPast);
-
-  let ageDeltaFuture = ((1/11.86) * (this.ageDelta - this.age)).toFixed(2);
-  this.ageDeltaFuture = parseFloat(ageDeltaFuture);
-  
-  return ageAlt;
-};
